db2e85653e17957cbe28a176619bc718245c69e5
Jingsheng Wang <skyred@insready.com>
2018-02-17 12:24:39 +0800

    Add git since PHP-7.2 doesn't have it anymore

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN apt-get update && apt-get install -y \
 	ca-certificates \
 	curl \
 	gnupg \
+	git \
 	--no-install-recommends
 
 # Add the vscode debian repo
--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 set -e
 set -o pipefail
 
-su user -c "code . --extensions-dir /var/vscode-ext"
-sleep infinity
+su user -p -c "/usr/share/code/code . --extensions-dir /var/vscode-ext"
